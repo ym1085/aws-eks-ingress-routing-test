055b5499046dde0bd89675eea08dd351d26efa70
YOUNGMINKIM <Youngmin1085@gmail.com>
2025-10-01 00:34:01 +0900

    fix: health check controller 추가
    1. ingress, deployment에서 health check 수행을 위해 추가

---
+++ user-service/src/main/java/com/eks/user/api/v1/HealthCheckController.java
@@ -0,0 +1,17 @@
+package com.eks.user.api.v1;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health-check")
+    public ResponseEntity<?> healthCheck() {
+        return ResponseEntity.status(HttpStatus.OK).body("ok");
+    }
+}
